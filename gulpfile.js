9ae3b3d0e13ec7180cd4af4ff1d52ae005f3eed1
Paul Tibbetts <email@paultibbetts.uk>
2016-02-22 01:13:24 +0000

    Added Articles, Notes & Links, About & Contact pages

--- gulpfile.js
+++ gulpfile.js
@@ -62,7 +62,7 @@ gulp.task('styles:fabricator', function () {
 	gulp.src(config.src.styles.fabricator)
 		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
-		.pipe(prefix('last 1 version'))
+		.pipe(prefix('last 2 versions'))
 		.pipe(gulpif(!config.dev, csso()))
 		.pipe(rename('f.css'))
 		.pipe(sourcemaps.write())
